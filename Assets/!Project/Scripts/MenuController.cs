9d649d654dcd806dab91ab4a4d3b5f54d18b8622
Alfamodik <Salnikovdmitry.2006@gmail.com>
2024-03-24 15:16:31 +0500

    show questions BugFix

--- Assets/!Project/Scripts/MenuController.cs
+++ Assets/!Project/Scripts/MenuController.cs
@@ -2,68 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuController : MonoBehaviour {
-	[SerializeField] private GameObject startMenu;
-	[SerializeField] private GameObject selectCategory;
-	[SerializeField] private GameObject questions;
-	[SerializeField] private GameObject results;
-	[SerializeField] private GameObject pauseMenu;
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject startMenu;
+    [SerializeField] private GameObject selectCategory;
+    [SerializeField] private GameObject questions;
+    [SerializeField] private GameObject results;
+    [SerializeField] private GameObject pauseMenu;
 
-    public ScreenFadeScript screenFadeScript;
-
-    private void Start() {
-
-        screenFadeScript = GetComponent<ScreenFadeScript>();
-
+    private void Start()
+    {
         GameController.instance.onChangeState += ChangeState;
-		GroupButton[] groupButtons = GetComponentsInChildren<GroupButton>(true);
-		for(int i = 0; i < groupButtons.Length; i++) {
-			groupButtons[i].Init();
-		}
- 	}
+        GroupButton[] groupButtons = GetComponentsInChildren<GroupButton>(true);
+        for(int i = 0; i < groupButtons.Length; i++)
+        {
+            groupButtons[i].Init();
+        }
+    }
 
-	public void ShowCategory() {
-		startMenu.SetActive(false);
-		selectCategory.SetActive(true);
-	}
+    public void ShowCategory()
+    {
+        startMenu.SetActive(false);
+        selectCategory.SetActive(true);
+    }
 
-	private void ChangeState(GameState oldState, GameState newState) {
-		switch (oldState) {
-			case GameState.Menu:
-                screenFadeScript.FadeScreen(true); // Затемнение Экрана;
+    private void ChangeState(GameState oldState, GameState newState)
+    {
+        switch(oldState)
+        {
+            case GameState.Menu:
                 startMenu.SetActive(false);
-				selectCategory.SetActive(false);
-				break;
-			case GameState.Game:
-				if (oldState != GameState.Game || newState == GameState.Result)
-                    screenFadeScript.FadeScreen(true);
-                questions.SetActive(false);
-				break;
-			case GameState.Pause:
-				if (newState == GameState.Menu)
-					questions.SetActive(false);
-				pauseMenu.SetActive(false);
-				break;
-			case GameState.Result:
-				results.SetActive(false);
-				break;
-		}
-		switch (newState) {
-			case GameState.Menu:
-				startMenu.SetActive(true);
-				break;
-				questions.SetActive(true);
-				break;
-			case GameState.Result:
-				results.SetActive(true);
-				break;
-			case GameState.Pause:
-				pauseMenu.SetActive(true);
-				break;
-		}
-	}
+                selectCategory.SetActive(false);
+                break;
 
-	private void OnDestroy() {
-		GameController.instance.onChangeState -= ChangeState;
-	}
+            case GameState.Game:
+                if(oldState != GameState.Game || newState == GameState.Result)
+                    questions.SetActive(false);
+                break;
+
+            case GameState.Pause:
+                if(newState == GameState.Menu)
+                    questions.SetActive(false);
+
+                pauseMenu.SetActive(false);
+                break;
+
+            case GameState.Result:
+                results.SetActive(false);
+                break;
+        }
+        switch(newState)
+        {
+            case GameState.Menu:
+                startMenu.SetActive(true);
+                break;
+            case GameState.Game:
+                questions.SetActive(true);
+                break;
+            case GameState.Result:
+                results.SetActive(true);
+                break;
+            case GameState.Pause:
+                pauseMenu.SetActive(true);
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameController.instance.onChangeState -= ChangeState;
+    }
 }
