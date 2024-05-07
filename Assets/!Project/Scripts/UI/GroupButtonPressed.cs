43b7db076171685911d57b06131cde48eb64586f
Alfamodik <Salnikovdmitry.2006@gmail.com>
2024-05-07 17:55:41 +0500

    Add delay and YG plagin

--- Assets/!Project/Scripts/UI/GroupButtonPressed.cs
+++ Assets/!Project/Scripts/UI/GroupButtonPressed.cs
@@ -1,31 +1,35 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GroupButtonPressed : MonoBehaviour
 {
-    [SerializeField] private int id;
-	[SerializeField] private Button button;
-
+    public const float OnClickDelay = 0.15f;
     public Action<int> onButtonClick;
 
-	private void Start() {
-		button = GetComponent<Button>();
-		button.onClick.AddListener(OnClick);
-	}
+    [SerializeField] private int id;
+    [SerializeField] private Button button;
 
-	private void OnClick() {
-		onButtonClick?.Invoke(id);
-	}
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
 
-	public void SetId(int id) {
-		this.id = id;
-	}
+    private void OnDestroy()
+    {
+        if(button)
+            button.onClick.RemoveListener(OnClick);
+    }
 
-	private void OnDestroy() {
-		if (button)
-			button.onClick.RemoveListener(OnClick);
-	}
+    public void SetId(int id) => this.id = id;
+
+    private void OnClick() => StartCoroutine(Invoke(OnClickDelay));
+
+    private IEnumerator Invoke(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        onButtonClick?.Invoke(id);
+    }
 }
